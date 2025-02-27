87cfa55fc3f2e515a63440b67dd7a2cd2d51f22b
Ian Page Maciel <ianpage27@gmail.com>
2025-02-27 19:19:50 -0300

    axercicio

--- altura-peso/src/componentes/calculo/index.jsx
+++ altura-peso/src/componentes/calculo/index.jsx
@@ -1,16 +1,19 @@
 import { useState } from "react"
 
+
+
+
 const Calculo = () => {
-        const [altura, setAltura] = useState(0)
-        const [peso, setPeso] = useState(0)
-        const [resultado, setResultado] = useState(0)
+        const [altura, setAltura] = useState(null)
+        const [peso, setPeso] = useState(null)
+        const [resultado, setResultado] = useState(null)
 
 
     const calcularIMC = () =>{
         
         const imc = peso / (altura * altura)
-        
-        if(altura === 0 || peso === 0){
+        // isNaN (altura) e isNan(peso) foram adicionados. Serve para verificar se a variável altura ou a variável peso são números válidos isNaN=(is Not a Number).
+        if( isNaN(altura) || isNaN(peso) ||altura === 0 || peso === 0){
             setResultado(
             <div>
             <p>Erro, Por favor insira novamente os dados e tente novamente</p>
@@ -30,11 +33,11 @@ const Calculo = () => {
     }
     
     
-
+    //nos inputs foi adicionado o o (value)  garante que o input de altura sempre exiba o valor armazenado na variável de estado altura ou peso 
     return(
-        <div>
-            <input type="number" placeholder="Altura (Metros)" onChange={evento => {setAltura(parseFloat(evento.target.value))}} />
-            <input type="number" name="" id="" placeholder="Peso (KG)" onChange={evento => {setPeso(parseFloat(evento.target.value))}}/>
+        <div >
+            <input type="number" placeholder="Altura (Metros)" value={altura} onChange={evento => {setAltura(parseFloat(evento.target.value))}} />
+            <input type="number"  placeholder="Peso (KG)" value={peso} onChange={evento => {setPeso(parseFloat(evento.target.value))}}/>
             <button type="button" onClick={calcularIMC}>Calcular</button>
             {resultado}
         </div>
